6e66c272c5a4be85382d0e89bf944445420cf8df
calebkloeckner <calebkloeckner@gmail.com>
2018-02-08 09:13:46 -0700

    Items displayed as a list

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,23 +8,41 @@ const connection = mysql.createConnection({
     password:'root',
     database: 'bamazon'
 });
-connection.connect(function(err) {
-    if (err) throw err;
-    con.query("SELECT * FROM products", function(err, result) {
-        if (err) throw err;
-        console.log(result);
-    });
-    // runSearch();
+connection.connect(function(err){
+    if(err) throw err;
+    console.log("Get ready to buy from Bamazon");
+    listOfProducts();
+
 });
 
-// function runSearch() {
-//     inquirer
-//       .prompt({
-//         name: "action",
-//         type: "list",
-//         message: "What would you like to do?",
-//         choices: [
-          
-//         ]
-//       })
-//     }
+var itemsBought = [];
+// lists all of the products available
+function listOfProducts() {
+    connection.query("SELECT * FROM products", function(err, res){
+        inquirer
+        .prompt({
+          name: "action",
+          type: "list",
+          message: "What item tickles your fancy? Select the item you are interested in:",
+          choices: function() {
+              var choices = [];
+              for(var i = 0; i < res.length; i++){
+                  choices.push("Item number: " + res[i].item_id + " - " + res[i].product_name + ": Cost: $" + res[i].price);
+              }
+              return choices;
+          }
+        
+    }).then(function(answer){
+        var chosenItem;
+        for(var i = 0; i < res.length; i++){
+            if(res[i].product_name === answer.items){
+                chosenItem = res[i];
+            }
+        }
+        itemsBought.push(chosenItem);
+       
+        purchaseQuanity();
+    });
+
+});
+}
